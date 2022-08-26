eecb5b61b7f8c44c3a931c97d5c9dd6d0f65682d
amarouldhamadouche <amarouldhamadouche0@gmail.com>
2022-08-26 14:37:34 +0100

    my first commit

--- firebase.js
+++ firebase.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { auth } from "firebase/auth";
 import firebase from "firebase/app";
 const firebaseConfig = {
-  apiKey: "AIzaSyCoTB6jdhUlLxBhClN9An0v-bt_X_uzYSw",
-  authDomain: "otpauth-7f659.firebaseapp.com",
-  projectId: "otpauth-7f659",
-  storageBucket: "otpauth-7f659.appspot.com",
-  messagingSenderId: "862405200725",
-  appId: "1:862405200725:web:a16028e6960da54e2dfddc",
+ur firebase configuration
 };
 
 // Initialize Firebase
